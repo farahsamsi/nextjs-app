f385cefe8b646cf741bfdf15287e629463318860
farahsamsi <1827034farahsamsiprome@gmail.com>
2025-05-28 22:07:11 +0600

    linking and navigation basics

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,29 @@
+"use client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 export default function AboutPage() {
+  // for logical routing
+  const router = useRouter();
+  const isLoggedIn = false;
+  const handleNavigation = () => {
+    if (isLoggedIn) {
+      router.push("/about/address");
+    } else {
+      router.push("/");
+    }
+  };
+
   return (
     <div>
       <p className="font-bold text-red-800 text-4xl">About Page</p>
+      <p>
+        <Link href="/about/address">Address</Link>
+      </p>
+      <button type="button" onClick={handleNavigation} className="">
+        Address Page
+      </button>
     </div>
   );
 }
