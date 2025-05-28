f43d5b4c48d744ec7b83a81ba94de50dea1f600f
farahsamsi <1827034farahsamsiprome@gmail.com>
2025-05-28 21:56:07 +0600

    basic routing creating folder and file named page

---
+++ src/app/about/address/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AddressPage() {
+  return (
+    <div>
+      <p className="font-bold text-blue-800 text-4xl">Address Page</p>
+    </div>
+  );
+}
---
+++ src/app/about/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AboutPage() {
+  return (
+    <div>
+      <p className="font-bold text-red-800 text-4xl">About Page</p>
+    </div>
+  );
+}
